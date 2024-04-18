60b5e8606430f0c9edb282415936202d16c88ef5
nferhat <nferhat20@gmail.com>
2024-04-18 17:27:08 +0100

    fht-compositor: Implement regex-based window rules

--- fht-compositor/src/config/types/rules.rs
+++ fht-compositor/src/config/types/rules.rs
@@ -1,4 +1,5 @@
-use serde::{Deserialize, Serialize};
+use regex::Regex;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 use crate::shell::FhtWindow;
 
@@ -6,7 +7,25 @@ const fn default_true() -> bool {
     true
 }
 
-#[derive(Default, Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
+fn serialize_regex<S: Serializer>(regex: &Option<Regex>, serializer: S) -> Result<S::Ok, S::Error> {
+    if let Some(regex) = regex {
+        let regex_str = regex.to_string();
+        serializer.serialize_str(&regex_str)
+    } else {
+        serializer.serialize_none()
+    }
+}
+
+fn deserialize_regex<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> Result<Option<Regex>, D::Error> {
+    let regex_string = String::deserialize(deserializer)?;
+    Regex::new(&regex_string).map(Some).map_err(|err| {
+        <D::Error as serde::de::Error>::custom(format!("Invalid regex string! {err}"))
+    })
+}
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
 pub struct WindowRulePattern {
     /// The workspace index the window is getting spawned on.
     #[serde(default)]
@@ -15,27 +34,79 @@ pub struct WindowRulePattern {
     /// The window title regex to match on
     ///
     /// NOTE: The compositor checks before for a title since it's more specific than an app id.
-    #[serde(default)]
-    title: Option<String>,
+    #[serde(
+        default,
+        serialize_with = "serialize_regex",
+        deserialize_with = "deserialize_regex"
+    )]
+    title: Option<Regex>,
 
     /// The app id regex to match on.
     ///
     /// This is commonly known as the window CLASS, or WM_CLASS on X.org
-    #[serde(default)]
-    app_id: Option<String>,
+    #[serde(
+        default,
+        serialize_with = "serialize_regex",
+        deserialize_with = "deserialize_regex"
+    )]
+    app_id: Option<Regex>,
+}
+
+impl std::hash::Hash for WindowRulePattern {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        if let Some(workspace) = self.workspace {
+            state.write_usize(workspace)
+        }
+        if let Some(title_regex) = &self.title {
+            for byte in title_regex.as_str().bytes() {
+                state.write_u8(byte)
+            }
+        }
+        if let Some(app_id_regex) = &self.app_id {
+            for byte in app_id_regex.as_str().bytes() {
+                state.write_u8(byte)
+            }
+        }
+    }
+}
+
+impl PartialEq for WindowRulePattern {
+    fn eq(&self, other: &Self) -> bool {
+        self.workspace == other.workspace
+            && regex_matches(self.title.as_ref(), other.title.as_ref())
+            && regex_matches(self.app_id.as_ref(), other.app_id.as_ref())
+    }
+}
+
+impl Eq for WindowRulePattern {}
+
+fn regex_matches(regex_1: Option<&Regex>, regex_2: Option<&Regex>) -> bool {
+    regex_1.map(Regex::as_str) == regex_2.map(Regex::as_str)
 }
 
 impl WindowRulePattern {
     pub fn matches(&self, window: &FhtWindow, workspace: usize) -> bool {
-        if let Some(&workspace_idx) = self.workspace.as_ref() {
-            workspace_idx == workspace
-        } else if let Some(title) = self.title.as_ref() {
-            &window.title() == title
-        } else if let Some(app_id) = self.app_id.as_ref() {
-            &window.app_id() == app_id
-        } else {
-            false
+        if self.workspace.as_ref().is_some_and(|ws| workspace == *ws) {
+            return true;
         }
+
+        if self
+            .title
+            .as_ref()
+            .is_some_and(|regex| regex.is_match(&window.title()))
+        {
+            return true;
+        }
+
+        if self
+            .app_id
+            .as_ref()
+            .is_some_and(|regex| regex.is_match(&window.app_id()))
+        {
+            return true;
+        }
+
+        false
     }
 }
 
