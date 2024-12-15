4d781b47ae8e6f3eb1fab326888cfc919559ac4c
nferhat <nferhat20@gmail.com>
2024-12-15 08:22:56 +0100

    fix: Implement proper pointer constraints
    The previous implementation was directly ripped off anvil, which didn't
    take into consideration the window position.
    
    I noticed this when working in blender. After running this commit it
    seems like the locked position hint is properly accounted for

--- src/handlers/pointer_constraints.rs
+++ src/handlers/pointer_constraints.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 use smithay::delegate_pointer_constraints;
 use smithay::input::pointer::PointerHandle;
 use smithay::reexports::wayland_server::protocol::wl_surface::WlSurface;
@@ -32,15 +34,25 @@ impl PointerConstraintsHandler for State {
         if with_pointer_constraint(surface, pointer, |constraint| {
             constraint.map_or(false, |c| c.is_active())
         }) {
-            let origin = self
-                .fht
-                .space
-                .find_window(surface)
-                .map(|w| w.render_offset())
-                .unwrap_or_default()
-                .to_f64();
+            let Some(focused_surface) = pointer
+                .current_focus()
+                .and_then(|f| f.wl_surface().map(Cow::into_owned))
+            else {
+                return;
+            };
 
-            pointer.set_location(origin + location);
+            if &focused_surface != surface {
+                // only focused surfaec can give position hint
+                // this is to avoid random cursor warps around the screen
+                return;
+            }
+
+            // TODO: cursor_position_hint for layer surfaces?
+            let Some(window) = self.fht.space.find_window(surface) else {
+                return;
+            };
+            let window_loc = self.fht.space.window_location(&window).unwrap();
+            pointer.set_location(window_loc.to_f64() + window.render_offset().to_f64() + location);
         }
     }
 }
