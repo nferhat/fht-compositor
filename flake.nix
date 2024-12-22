f4827bc5a251c37d73fdbc448fe81275c8ab2ed2
nferhat <nferhat20@gmail.com>
2024-12-22 09:08:52 +0100

    flake: Add home-manager module
    Really simple one at that. Just enable the compositor and specify some
    settings. Since the configuration is toml, we can pretty much map the
    config one-to-one

--- flake.nix
+++ flake.nix
@@ -174,7 +174,7 @@
             enable = lib.mkEnableOption "fht-compositor";
             package = lib.mkOption {
               type = lib.types.package;
-              default = pkgs.callPackage fht-compositor-package { };
+              default = pkgs.callPackage fht-compositor-package {};
             };
           };
 
@@ -198,8 +198,7 @@
                 then {
                   displayManager.sessionPackages = [cfg.package];
                 }
-                else
-                {
+                else {
                   xserver.displayManager.sessionPackages = [cfg.package];
                 };
               # OpenGL/mesa is required. We do not have a software renderer.
@@ -245,6 +244,75 @@
               };
             })
           ];
+        };
+      };
+
+      flake.homeModules = rec {
+        default = fht-compositor;
+        # NOTE: This module implementation is directly ripped from home-manager's helix module
+        #   home-manager/modules/programs/helix.nix
+        # TODO: Maybe add a validation/test? I dont know how I can do it but we have the
+        # check-configuration subcommand, so maybe thats possible.
+        fht-compositor = {
+          lib,
+          config,
+          options,
+          pkgs,
+          ...
+        }: let
+          cfg = config.programs.fht-compositor;
+          tomlFormat = pkgs.formats.toml {};
+        in {
+          options.programs.fht-compositor = {
+            enable = lib.mkEnableOption "fht-compositor";
+
+            package = lib.mkOption {
+              type = lib.types.package;
+              default = pkgs.callPackage fht-compositor-package {};
+            };
+
+            settings = lib.mkOption {
+              type = tomlFormat.type;
+              default = {};
+              example = lib.literalExpression ''
+                {
+                  autostart = [];
+                  general.cursor-warps = true;
+
+                  decorations.border = {
+                    thickness = 3;
+                    radius = 0;
+                    focused-color = {
+                      start = "#5781b9";
+                      end = "7fc8db";
+                      angle = 0;
+                    };
+                  };
+
+                  animations.disable = false;
+
+                  keybinds."Super-q" = "quit";
+
+                  rules = [
+                    { on-workspace = 5; floating = true; centered = true };
+                    # other window rules...
+                  ]
+                }
+              '';
+              description = ''
+                Configuration written to
+                {file}`$XDG_CONFIG_HOME/fht/compositor.toml`.
+              '';
+            };
+          };
+
+          config = lib.mkIf cfg.enable {
+            home.packages = [cfg.package];
+            xdg.configFile.fht-compositor-config = lib.mkIf (cfg.settings != {}) {
+              target = "fht/compositor.toml";
+              source = tomlFormat.generate "fht-compositor-config" cfg.settings;
+            };
+          };
         };
       };
     };
