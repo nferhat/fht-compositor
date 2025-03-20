efe53eb348af0c04bad08a68654b21617c2ff2b3
nferhat <nferhat20@gmail.com>
2025-03-20 13:59:42 +0100

    window-rules: Add `ontop` property

--- fht-compositor-config/src/lib.rs
+++ fht-compositor-config/src/lib.rs
@@ -997,6 +997,7 @@ pub struct WindowRule {
     pub maximized: Option<bool>,
     pub fullscreen: Option<bool>,
     pub floating: Option<bool>,
+    pub ontop: Option<bool>,
     pub centered: Option<bool>, // only effective if floating == Some(true)
 }
 
