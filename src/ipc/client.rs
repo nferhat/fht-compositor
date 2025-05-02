f4de991db35920fa1a250d880b9c1d03c8efcfd9
nferhat <nferhat20@gmail.com>
2025-05-02 13:00:47 +0100

    ipc: Add `FocusedWindow` and `FocusedWorkspace` requests

--- src/ipc/client.rs
+++ src/ipc/client.rs
@@ -11,6 +11,8 @@ pub fn make_request(request: cli::Request, json: bool) -> anyhow::Result<()> {
         cli::Request::Outputs => fht_compositor_ipc::Request::Outputs,
         cli::Request::Windows => fht_compositor_ipc::Request::Windows,
         cli::Request::LayerShells => fht_compositor_ipc::Request::LayerShells,
+        cli::Request::FocusedWindow => fht_compositor_ipc::Request::FocusedWindow,
+        cli::Request::FocusedWorkspace => fht_compositor_ipc::Request::FocusedWorkspace,
         cli::Request::Space => fht_compositor_ipc::Request::Space,
         cli::Request::Action { action } => fht_compositor_ipc::Request::Action(action),
     };
@@ -43,6 +45,10 @@ pub fn make_request(request: cli::Request, json: bool) -> anyhow::Result<()> {
             fht_compositor_ipc::Response::Windows(windows) => serde_json::to_string(&windows),
             fht_compositor_ipc::Response::LayerShells(layer_shells) => {
                 serde_json::to_string(&layer_shells)
+            }
+            fht_compositor_ipc::Response::FocusedWindow(window) => serde_json::to_string(&window),
+            fht_compositor_ipc::Response::FocusedWorkspace(workspace) => {
+                serde_json::to_string(&workspace)
             }
             fht_compositor_ipc::Response::Space(space) => serde_json::to_string(&space),
             fht_compositor_ipc::Response::Error(err) => {
@@ -113,35 +119,7 @@ fn print_formatted(res: &fht_compositor_ipc::Response) -> anyhow::Result<()> {
         }
         fht_compositor_ipc::Response::Windows(windows) => {
             for window in windows {
-                writeln!(&mut writer, "Window #{}", window.id)?;
-                writeln!(&mut writer, "\tTitle: {:?}", window.title)?;
-                writeln!(&mut writer, "\tApplication ID: {:?}", window.app_id)?;
-                writeln!(&mut writer, "\tCurrent output: {}", window.output)?;
-                writeln!(
-                    &mut writer,
-                    "\tCurrent workspace index: {}",
-                    window.workspace_idx
-                )?;
-                writeln!(
-                    &mut writer,
-                    "\tCurrent workspace ID: {}",
-                    window.workspace_id
-                )?;
-                writeln!(
-                    &mut writer,
-                    "\tSize: ({}, {})",
-                    window.size.0, window.size.1
-                )?;
-                writeln!(
-                    &mut writer,
-                    "\tLocation: ({}, {})",
-                    window.location.0, window.location.1
-                )?;
-                writeln!(&mut writer, "\tFullscreened: {}", window.fullscreened)?;
-                writeln!(&mut writer, "\tMaximized: {}", window.maximized)?;
-                writeln!(&mut writer, "\tTiled: {}", window.tiled)?;
-                writeln!(&mut writer, "\tActivated: {}", window.activated)?;
-                writeln!(&mut writer, "\tFocused: {}", window.focused)?;
+                print_window(&mut writer, window)?;
                 writeln!(&mut writer, "---")?;
             }
         }
@@ -190,9 +168,51 @@ fn print_formatted(res: &fht_compositor_ipc::Response) -> anyhow::Result<()> {
             }
             //
         }
+        fht_compositor_ipc::Response::FocusedWindow(window) => match window.as_ref() {
+            Some(window) => print_window(&mut writer, window)?,
+            None => println!("No focused window"),
+        },
+        fht_compositor_ipc::Response::FocusedWorkspace(workspace) => {
+            writeln!(&mut writer, "ID: {}", workspace.id)?;
+            writeln!(&mut writer, "Windows: {:?}", workspace.windows)?;
+            writeln!(&mut writer, "Master width factor: {}", workspace.mwfact)?;
+            writeln!(
+                &mut writer,
+                "Number of master windows: {}",
+                workspace.nmaster
+            )?;
+        }
         fht_compositor_ipc::Response::Error(err) => anyhow::bail!(err.clone()),
         fht_compositor_ipc::Response::Noop => (),
     }
 
+    Ok(())
+}
+
+fn print_window(
+    writer: &mut impl std::io::Write,
+    window: &fht_compositor_ipc::Window,
+) -> std::io::Result<()> {
+    writeln!(writer, "Window #{}", window.id)?;
+    writeln!(writer, "\tTitle: {:?}", window.title)?;
+    writeln!(writer, "\tApplication ID: {:?}", window.app_id)?;
+    writeln!(writer, "\tCurrent output: {}", window.output)?;
+    writeln!(
+        writer,
+        "\tCurrent workspace index: {}",
+        window.workspace_idx
+    )?;
+    writeln!(writer, "\tCurrent workspace ID: {}", window.workspace_id)?;
+    writeln!(writer, "\tSize: ({}, {})", window.size.0, window.size.1)?;
+    writeln!(
+        writer,
+        "\tLocation: ({}, {})",
+        window.location.0, window.location.1
+    )?;
+    writeln!(writer, "\tFullscreened: {}", window.fullscreened)?;
+    writeln!(writer, "\tMaximized: {}", window.maximized)?;
+    writeln!(writer, "\tTiled: {}", window.tiled)?;
+    writeln!(writer, "\tActivated: {}", window.activated)?;
+    writeln!(writer, "\tFocused: {}", window.focused)?;
     Ok(())
 }
