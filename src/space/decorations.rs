9c53b74ce280f40a36b0ea609a19b6529b749a29
nferhat <nferhat20@gmail.com>
2025-07-07 20:32:36 +0100

    renderer(shaders): Use custom `ShaderElement`
    Instead of using a wrapper over smithay's PixelShaderElement
    we just make our own re-implementation of it, notably to add
    the possibility to change the opacity of the shader element
    on-the-fly.
    
    Right now this is not used, but in next commits, this will
    be used to refactor decorations

--- src/space/decorations.rs
+++ src/space/decorations.rs
@@ -6,8 +6,7 @@ use smithay::backend::renderer::element::Kind;
 use smithay::backend::renderer::gles::Uniform;
 use smithay::utils::{Logical, Point, Rectangle, Size};
 
-use crate::renderer::pixel_shader_element::FhtPixelShaderElement;
-use crate::renderer::shaders::Shaders;
+use crate::renderer::shaders::{ShaderElement, Shaders};
 use crate::renderer::AsGlowRenderer;
 
 pub fn draw_border(
@@ -18,37 +17,17 @@ pub fn draw_border(
     thickness: f64,
     radius: f64,
     color: fht_compositor_config::Color,
-) -> FhtPixelShaderElement {
+) -> ShaderElement {
     let scaled_thickness = thickness * scale as f64;
     let (start_color, end_color, angle) = match color {
         fht_compositor_config::Color::Solid(color) => (color, color, 0.0),
         fht_compositor_config::Color::Gradient { start, end, angle } => (start, end, angle),
     };
 
-    // Only draw whats needed for the border.
-    //
-    // XXXXXXXXXXXXXXXXXX
-    // XXooooooooooooooXX
-    // XooooooooooooooooX
-    // XooooooooooooooooX
-    // XooooooooooooooooX
-    // XooooooooooooooooX
-    // XXooooooooooooooXX
-    // XXXXXXXXXXXXXXXXXX
-    //
-    // We leave the four patches of X at each border for corner radius
-    // FIXME: const?
-    let border_damage = Rectangle::from_size(geometry.size).subtract_rect(Rectangle::new(
-        Point::from((scaled_thickness, scaled_thickness)).to_i32_round(),
-        geometry.size
-            - Size::from((scaled_thickness, scaled_thickness))
-                .upscale(2.0)
-                .to_i32_round(),
-    ));
-
-    FhtPixelShaderElement::new(
+    ShaderElement::new(
         Shaders::get(renderer.glow_renderer()).border.clone(),
         geometry,
+        None,
         alpha,
         vec![
             Uniform::new("v_start_color", start_color),
@@ -58,7 +37,6 @@ pub fn draw_border(
             Uniform::new("thickness", scaled_thickness as f32),
             Uniform::new("corner_radius", radius as f32),
         ],
-        Some(&border_damage),
         Kind::Unspecified,
     )
 }
@@ -73,31 +51,15 @@ pub fn draw_shadow(
     blur_sigma: f32,
     corner_radius: f32,
     color: [f32; 4],
-) -> FhtPixelShaderElement {
+) -> ShaderElement {
     let scaled_blur_sigma = (blur_sigma / scale as f32).round() as i32;
     geometry.loc -= Point::from((scaled_blur_sigma, scaled_blur_sigma));
     geometry.size += Size::from((2 * scaled_blur_sigma, 2 * scaled_blur_sigma));
 
-    // We only draw the shadow around the window, its pointless to damage everything, only causing
-    // useless drawing from the GPU.
-    //
-    // XXXXXXXXXXXXXXXXXX
-    // XXooooooooooooooXX
-    // XXooooooooooooooXX
-    // XXooooooooooooooXX
-    // XXooooooooooooooXX
-    // XXXXXXXXXXXXXXXXXX
-    //
-    // We generate the damage for the regions marked by X, relative to (0,0)
-    // FIXME: const?
-    let shadow_damage = Rectangle::from_size(geometry.size).subtract_rect(Rectangle::new(
-        Point::from((scaled_blur_sigma, scaled_blur_sigma)),
-        geometry.size - Size::from((scaled_blur_sigma, scaled_blur_sigma)).upscale(2),
-    ));
-
-    FhtPixelShaderElement::new(
+    ShaderElement::new(
         Shaders::get(renderer.glow_renderer()).box_shadow.clone(),
         geometry,
+        None,
         alpha,
         vec![
             // NOTE: For some reasons we cant use f64s, we shall cast
@@ -105,7 +67,6 @@ pub fn draw_shadow(
             Uniform::new("blur_sigma", blur_sigma),
             Uniform::new("corner_radius", corner_radius),
         ],
-        Some(&shadow_damage),
         Kind::Unspecified,
     )
 }
