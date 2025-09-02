5131b152016d3445442abe3107282cefea4e8cb4
Byson94 <byson94@outlook.com>
2025-09-02 12:47:36 +0530

    fix: fixed test_client compilation error

--- fht-compositor-ipc/test_client.rs
+++ fht-compositor-ipc/test_client.rs
@@ -1,9 +1,9 @@
-use std::io::{BufRead, BufReader, Read, Write};
+use std::io::{BufRead, BufReader, Write};
 use std::os::unix::net::UnixStream;
 
 use fht_compositor_ipc::{IpcRequest, Request, Response};
 
-fn write_req(stream: &mut UnixStream, req: Request) -> Response {
+fn write_req(stream: &mut UnixStream, req: IpcRequest) -> Response {
     let mut req = serde_json::to_string(&req).unwrap();
     req.push('\n'); // it is required to append a newline.
     stream.write_all(req.as_bytes()).unwrap();
