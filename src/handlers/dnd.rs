2e3bf0c31eb48b72b04668994834ed8b641ee96e
nferhat <nadjib.ferhat@etu.usthb.dz>
2026-02-01 11:43:19 +0100

    Switch smithay to `feat/fb-effects`

--- src/handlers/dnd.rs
+++ src/handlers/dnd.rs
@@ -1,36 +1,83 @@
-use smithay::reexports::wayland_server::protocol::wl_data_source::WlDataSource;
+use smithay::desktop::find_popup_root_surface;
+use smithay::input::dnd::{self, DnDGrab, DndGrabHandler};
+use smithay::input::pointer::Focus;
 use smithay::reexports::wayland_server::protocol::wl_surface::WlSurface;
-use smithay::wayland::selection::data_device::{ClientDndGrabHandler, ServerDndGrabHandler};
+use smithay::wayland::seat::WaylandFocus;
+use smithay::wayland::selection::data_device::WaylandDndGrabHandler;
 
+use crate::output::OutputExt;
 use crate::state::State;
 
-impl ClientDndGrabHandler for State {
-    fn started(
+impl WaylandDndGrabHandler for State {
+    fn dnd_requested<S: smithay::input::dnd::Source>(
         &mut self,
-        _source: Option<WlDataSource>,
+        source: S,
         icon: Option<WlSurface>,
-        _seat: smithay::input::Seat<Self>,
+        seat: smithay::input::Seat<Self>,
+        serial: smithay::utils::Serial,
+        type_: smithay::input::dnd::GrabType,
     ) {
         self.fht.dnd_icon = icon;
-    }
 
-    fn dropped(
-        &mut self,
-        _target: Option<WlSurface>,
-        _validated: bool,
-        _seat: smithay::input::Seat<Self>,
-    ) {
-        self.fht.dnd_icon = None;
+        match type_ {
+            dnd::GrabType::Pointer => {
+                let pointer = seat.get_pointer().unwrap();
+                let start_data = pointer.grab_start_data().unwrap();
+                let grab = DnDGrab::new_pointer(&self.fht.display_handle, start_data, source, seat);
+                pointer.set_grab(self, grab, serial, Focus::Keep);
+            }
+            dnd::GrabType::Touch => _ = (source, serial),
+        }
+
+        // FIXME: more granular
+        self.fht.queue_redraw_all();
     }
 }
 
-impl ServerDndGrabHandler for State {
-    fn send(
+impl DndGrabHandler for State {
+    fn dropped(
         &mut self,
-        _mime_type: String,
-        _fd: std::os::unix::prelude::OwnedFd,
+        target: Option<dnd::DndTarget<'_, Self>>,
+        validated: bool,
         _seat: smithay::input::Seat<Self>,
+        location: smithay::utils::Point<f64, smithay::utils::Logical>,
     ) {
-        unreachable!("We don't support server-side grabs");
+        // Focus the activated output.
+        let mut focus_output = false;
+        if let Some(target) = validated.then_some(target).flatten() {
+            // SAFETY: We only do Pointer grabs for now
+            let target_surface = target.into_inner().wl_surface().unwrap();
+            let root = self
+                .fht
+                .root_surfaces
+                .get(&*target_surface)
+                .cloned()
+                .or_else(|| {
+                    // Popups.
+                    self.fht
+                        .popups
+                        .find_popup(&*target_surface)
+                        .and_then(|p| find_popup_root_surface(&p).ok())
+                })
+                .unwrap_or_else(|| target_surface.into_owned());
+            if let Some(window) = self.fht.space.find_window(&root) {
+                self.fht.space.activate_window(&window, true);
+                self.fht.focused_on_demand_layer_shell = None;
+                focus_output = true;
+            }
+        }
+
+        if focus_output {
+            let output = self
+                .fht
+                .space
+                .outputs()
+                .find(|o| o.geometry().to_f64().contains(location))
+                .cloned();
+
+            if let Some(output) = output {
+                self.fht.space.set_active_output(&output);
+            }
+        }
     }
 }
--- src/handlers/virtual_keyboard.rs
+++ src/handlers/virtual_keyboard.rs
@@ -1,1 +1,29 @@
+use smithay::wayland::virtual_keyboard::VirtualKeyboardHandler;
+
+use crate::state::State;
+
+// FIXME: IMPL
+
+impl VirtualKeyboardHandler for State {
+    fn on_keyboard_event(
+        &mut self,
+        keycode: smithay::backend::input::Keycode,
+        state: smithay::backend::input::KeyState,
+        time: u32,
+        keyboard: smithay::input::keyboard::KeyboardHandle<Self>,
+    ) {
+        _ = (keycode, state, time, keyboard);
+    }
+
+    fn on_keyboard_modifiers(
+        &mut self,
+        depressed_mods: smithay::input::keyboard::xkb::ModMask,
+        latched_mods: smithay::input::keyboard::xkb::ModMask,
+        locked_mods: smithay::input::keyboard::xkb::ModMask,
+        keyboard: smithay::input::keyboard::KeyboardHandle<Self>,
+    ) {
+        _ = (depressed_mods, latched_mods, locked_mods, keyboard);
+    }
+}
+
 smithay::delegate_virtual_keyboard_manager!(crate::state::State);
